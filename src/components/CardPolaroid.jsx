f0d33dfc90930a1c500b270639709b42ab2dcbd1
MattiasObregon <mattiasobregon@gmail.com>
2023-08-08 18:53:27 -0300

    rrd

--- src/components/CardPolaroid.jsx
+++ src/components/CardPolaroid.jsx
@@ -1,8 +1,10 @@
+import { Link as Anchor } from 'react-router-dom'
+
 export default function CardPolaroid({item}) {
   return (
-    <div key={item.id} className="w-[308px] h-[200px] relative">
+    <Anchor to={`/city/${item.id}`} key={item.id} className="w-[308px] h-[200px] relative">
         <img src={item.photo} alt={item.id} className="w-full h-full object-cover rounded-md" />
         <p className="absolute bottom-2 left-2 text-white font-semibold">{item.city}</p>
-    </div>
+    </Anchor>
   )
 }
