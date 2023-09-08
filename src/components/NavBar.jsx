2c0c7d0b467a539d5587a886c38feb685d445d78
Matias Obregon <mattiasobregon@gmail.com>
2023-09-08 02:43:26 -0300

    auth (#7)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
-import ToggleMenu from './ToggleMenu';
-import Display from './Display';
-import DisplayResponsive from './DisplayResponsive';
+import React, { useState } from 'react'
+
+import ToggleMenu from './ToggleMenu'
+import Display from './Display'
+import DisplayResponsive from './DisplayResponsive'
+import { useSelector, useDispatch } from 'react-redux/es/exports'
+import users_actions from '../store/actions/users'
+const { signout } = users_actions
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -10,15 +14,21 @@ export default function NavBar() {
     {to: "/cities", title: "Cities"},
     {to: "/auth/signin", title: "Login"},
   ]
-
+  
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+  
+    let mail = useSelector(store=>store.users.user?.mail)
+    let dispatch = useDispatch()
 
   return (
     <header className="container mx-auto flex flex-col md:flex-row justify-between items-center text-black">
 
-      <div className="text-2xl font-bold font-segoe-ui sm:hidden md:block">My Tinerary</div>
+      <div className="text-2xl font-bold font-segoe-ui sm:hidden md:block">My Tinerary - {mail}
+      <div></div>
+      { mail ? <span onClick={()=>dispatch(signout())} >SignOut</span> : null }
+      </div>
 
       <nav className="flex flex-col items-center text-center">
       <ToggleMenu onClick={toggleMenu} />
